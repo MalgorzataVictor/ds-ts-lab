88d922e4fead101ae97fc4085e97f496e1c4bdd2
Malgorzata Victor <malgorzatavictor@gmail.com>
2026-02-02 15:45:26 +0000

    Demo optional properties and parameters.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -2,8 +2,11 @@
 export interface Friend {
     name: string;
     phone: string;
-    age: number
+    dob? : Date;  
+    age: number;
+    interests? : string[] 
 }
+
 
 export interface Colleague {
     name: string;
